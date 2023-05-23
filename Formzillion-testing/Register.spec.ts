ac60fba66b86b67d693d83f93c2e4a7ea7e10ea3
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-05-23 16:26:25 +0530

    Update Register.spec.ts

--- Formzillion-testing/Register.spec.ts
+++ Formzillion-testing/Register.spec.ts
@@ -6,7 +6,14 @@ test.describe('Registeration testing', () => {
   test('with hardcoded email and password', async ({ page }) => {
     //path for registeration
     await page.goto('https://dev-app.formzillion.com/register');
-
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').press('Control+a');
+    await page.locator('input[type="email"]').fill('');
+    await page.locator('input[type="password"]').click();
+    await page.locator('input[type="password"]').press('Control+a');
+    await page.locator('input[type="password"]').fill('Qwerty@123');
+    await page.getByRole('checkbox').check();
+    await page.locator('button[type="submit"]').click();
   })
   
 });
