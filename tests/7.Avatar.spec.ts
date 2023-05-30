0ff3045da54947e1d719521e0cf11922b2923d7b
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-05-30 16:06:18 +0530

    test case for deleting the personal team deletion

--- tests/7.Avatar.spec.ts
+++ tests/7.Avatar.spec.ts
@@ -22,7 +22,11 @@ test.describe("personal Avatar Updating", async () => {
     ]);
 
     await page.goto(
-      `${process.env.NEXT_PUBLIC_APP_URL}/${process.env.NEXT_PUBLIC_USER_NAME}/settings`.replace(/"/g,""));
+      `${process.env.NEXT_PUBLIC_APP_URL}/${process.env.NEXT_PUBLIC_USER_NAME}/settings`.replace(
+        /"/g,
+        ""
+      )
+    );
     await page.reload();
     await page
       .getByRole("link", { name: "Settings" })
@@ -31,12 +35,19 @@ test.describe("personal Avatar Updating", async () => {
       .getByRole("listitem")
       .filter({ hasText: "General" })
       .click({ timeout: 20000 });
-      await page.getByLabel('Change User photo').click();
-      //await page.getByLabel('Change User photo').setInputFiles('Screenshot 2023-05-19 180659.png');
-      await page.locator('div').filter({ hasText: /^While an avatar is not mandatory, it is highly advisable to have one\. Save$/ }).getByRole('button', { name: 'Save' });
+    await page.getByLabel("Change User photo").click();
+    //await page.getByLabel('Change User photo').setInputFiles('Screenshot 2023-05-19 180659.png');
+    await page
+      .locator("div")
+      .filter({
+        hasText:
+          /^While an avatar is not mandatory, it is highly advisable to have one\. Save$/,
+      })
+      .getByRole("button", { name: "Save" });
   });
-  test('Team avatar testing', async({page})=>{
-    await page.goto(`${process.env.NEXT_PUBLIC_APP_URL}/login`.replace(/"/g, "")
+  test("Team avatar testing", async ({ page }) => {
+    await page.goto(
+      `${process.env.NEXT_PUBLIC_APP_URL}/login`.replace(/"/g, "")
     );
     await page
       .locator('input[type="email"]')
@@ -49,7 +60,12 @@ test.describe("personal Avatar Updating", async () => {
       page.waitForNavigation(),
       page.click('button[type="submit"]'),
     ]);
-    await page.goto(`${process.env.NEXT_PUBLIC_APP_URL}/${process.env.NEXT_PUBLIC_TEAM_NAME}`.replace(/"/g, ""));
+    await page.goto(
+      `${process.env.NEXT_PUBLIC_APP_URL}/${process.env.NEXT_PUBLIC_TEAM_NAME}`.replace(
+        /"/g,
+        ""
+      )
+    );
     await page.reload();
     await page
       .getByRole("link", { name: "Settings" })
@@ -58,7 +74,13 @@ test.describe("personal Avatar Updating", async () => {
       .getByRole("listitem")
       .filter({ hasText: "General" })
       .click({ timeout: 20000 });
-      await page.getByLabel('Change User photo').click();
-      await page.locator('div').filter({ hasText: /^While an avatar is not mandatory, it is highly advisable to have one\. Save$/ }).getByRole('button', { name: 'Save' });
-  })
+    await page.getByLabel("Change User photo").click();
+    await page
+      .locator("div")
+      .filter({
+        hasText:
+          /^While an avatar is not mandatory, it is highly advisable to have one\. Save$/,
+      })
+      .getByRole("button", { name: "Save" });
+  });
 });
